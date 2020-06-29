3d64e7f26f63a67bea0a401042030a1a18ce2279
miltonfelga <47542851+miltonfelga@users.noreply.github.com>
2020-06-29 00:51:19 -0300

    lista não aparece e reformar para filme

---
+++ app/src/main/java/com/example/appvolleyjsonarrayrequest/Contatos.java
@@ -0,0 +1,30 @@
+package com.example.appvolleyjsonarrayrequest;
+
+public class Contatos {
+    private String nome;
+    private String email;
+
+    public Contatos() {
+    }
+
+    public Contatos(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
